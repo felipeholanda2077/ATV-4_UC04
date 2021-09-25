54a8f3661edd13a80f0dae908fc76eb069a5cbe0
felipeholanda2077 <felipe_holandafreitas@hotmail.com>
2021-09-25 15:11:46 -0300

    ATV-4

---
+++ sql.sql
@@ -0,0 +1,19 @@
+FASE 1
+--------------------------------
+CREATE DATABASE SUPER;
+
+create table Produtos(
+   idProduto int AUTO_INCREMENT,
+   nomeProduto varchar(200),
+   valorProduto double(9,2),
+   quantidadeProduto int,
+   PRIMARY KEY(id)
+)
+
+FASE 2
+---------------------------------
+SELECT * FROM fornecedores;
+
+INSERT INTO Produtos (nomeProduto, valorProduto, quantidadeProduto, setor) VALUES ('Leite integral', '3', '100')
+
+INSERT INTO Produtos (nomeProduto, valorProduto, quantidadeProduto) values ('Leite integral', 4, 100);
